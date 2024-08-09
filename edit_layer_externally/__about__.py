e70018746f5da75daeada6d7e814143e29b033fb
rjmoerland <55446628+rjmoerland@users.noreply.github.com>
2024-08-09 10:30:37 +0200

    add __about__.py and tag v 0.1.0

---
+++ edit_layer_externally/__about__.py
@@ -0,0 +1,7 @@
+__title__ = "edit_layer_externally"
+__version__ = "0.1.0"
+__summary__ = "Krita plugin to run external application on a layer"
+__url__ = "https://github.com/rjmoerland/krita-edit-layer"
+
+__author__ = "R. J. Moerland"
+__copyright__ = "2024, " + __author__
